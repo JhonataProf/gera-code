fda7219d97efc3d33e4b7b6ff96d203198c42b2c
Jhonata <jhonata.sobral@ms.senac.br>
2024-10-22 19:53:01 -0400

    modularizando o script main

--- main.py
+++ main.py
@@ -1,47 +1,17 @@
-from pathlib import Path
-import os
-import subprocess
+from manager_dir import change_to_dir, create_dir, create_file, change_to_user_dir
+from execute_bash import atualizar_linux, instalar_pip, criar_venv, abrir_projeto_vscode
 
-# BUSCAR NOME DA PASTA DE USUÁRIO
-diretorio_home = Path.home()
-# ENTRAR NA PASTA DE USUÁRIO
-os.chdir(diretorio_home)
-
-# VARIAVÉL PARA DIRETÓRIO PADRÃO DOS PROJETOS
+diretorio_home = change_to_user_dir()
 diretorio_projetos = f'{diretorio_home}/Projetos'
-
-# VERIFICA SE EXISTE O DIRETÓRIO PADRÃO CASO NÃO ELE O CRIA
-if os.path.exists(diretorio_projetos) == False:
-    os.mkdir(diretorio_projetos)
-
-# ENTRA NO DIRETÓRIO PADRÃO
-os.chdir(diretorio_projetos)
-
-# SOLICITA AO USUÁRIO UM NOME PARA O PROJETO A SER CRIADO
+create_dir(diretorio_projetos)
+change_to_dir(diretorio_projetos)
 dir_novo_projeto = input('Digite o nome do Projeto a ser criado: ')
-
-# VARIÁVEL COM O CAMINHO COMPLETO DO PROJETO A SER CRIADO
 path_full_novo_projeto = f'{diretorio_projetos}/{dir_novo_projeto}'
-
-# CASO NÃO EXISTA O DIRETÓRIO CRIA
-if os.path.exists(path_full_novo_projeto) == False:
-    os.mkdir(path_full_novo_projeto)
-
-# ENTRA NO DIRETÓRIO
-os.chdir(path_full_novo_projeto)
-
-# VERIFICA SE NA RAIZ DO PROJETO EXISTE UM ARQUIVO MAIN
-# CASO NÃO ELE É CRIADO
-if os.path.isfile(f'{path_full_novo_projeto}/main.py') == False:
-    os.mknod('main.py')
-
-# EXIBE O CAMINHO COMPLET DO NOVO PROJETO CRIADO
+create_dir(path_full_novo_projeto)
+change_to_dir(path_full_novo_projeto)
+create_file(path_full_novo_projeto, "main.py")
 print(f'{path_full_novo_projeto}/main.py')
-
-subprocess.run(f'''sudo apt update -y && sudo apt-get upgrade -y''', shell=True, check=True, executable='/bin/bash')
-subprocess.run(f'''sudo apt install python3-pip''', shell=True, check=True, executable='/bin/bash')
-subprocess.run(f'''sudo apt install python3-venv''', shell=True, check=True, executable='/bin/bash')
-subprocess.run(f'''python3 -m venv .venv''', shell=True, check=True, executable='/bin/bash')
-subprocess.run(f'''source .venv/bin/activate''', shell=True, check=True, executable='/bin/bash')
-# ABRE O VS CODE NA PASTA DO PROJETO NOVO CRIADO
-subprocess.run(f'''code {path_full_novo_projeto}''', shell=True, check=True, executable='/bin/bash')
+atualizar_linux()
+instalar_pip()
+criar_venv()
+abrir_projeto_vscode(path_full_novo_projeto)
